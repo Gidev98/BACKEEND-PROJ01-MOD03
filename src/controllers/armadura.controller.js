0222528bdbab72811833655d73e35278a40a3498
sirdav1d <ddavid.diniz@gmail.com>
2022-04-14 04:10:19 -0300

    up

--- src/controllers/armadura.controller.js
+++ src/controllers/armadura.controller.js
@@ -1,39 +1,84 @@
-const armaduraService = require('../services/armadura.service') 
+const armaduraService = require('../services/armadura.service');
 
+const findAllArmadurasController = (req, res) => {
+  const armaduras = armaduraService.findAllArmaduraService();
 
-const findAllArmadurasController = (req, res) =>{
-    const armaduras = armaduraService.findAllArmaduraService();
-    res.send(armaduras)
-}
-
+  if(armaduras.length == 0){
+      return res.status(404).send({ message: 'Não existe Armadura Sagrada cadastrada!' });
+  }
+  res.send(armaduras);
+};
 
 const findByIdArmaduraController = (req, res) => {
-    const idParam = req.params.id;
-    const idArmadura = armaduraService.findByIdArmaduraService(idParam);
+  const idParam = +req.params.id;
 
-    res.send(idArmadura);
-}
+  if(!idParam){
+       return res.status(400).send({ message: 'ID inválido' });
+  }
+  const idArmadura = armaduraService.findByIdArmaduraService(idParam);
 
-const createArmaduraController = (req, res) =>{
-    const armadura = req.body;
-    const newArmadura = armaduraService.createArmaduraService(armadura);
-    res.send(newArmadura)
-}
+  if (!idArmadura) {
+    return res.status(404).send({ message: 'Armadura não encontrada!' });
+  }
+  
+  res.send(idArmadura);
+};
+
+const createArmaduraController = (req, res) => {
+  const armadura = req.body;
+
+  if (
+    !armadura ||
+    !armadura.nome ||
+    !armadura.descrcao ||
+    !armadura.usuarios ||
+    !armadura.background ||
+    !armadura.imagem
+  ) {
+    return res.status(400).send({ message: 'Objeto incompleto!' });
+  }
+  const newArmadura = armaduraService.createArmaduraService(armadura);
+  res.status(201).send(newArmadura);
+};
 
 const updateArmaduraController = (req, res) => {
-    const idParam = Number(req.params.id)
-    const armaduraEdit = req.body;
-    const updateArmadura = armaduraService.updateArmaduraService(idParam, armaduraEdit);
+  const idParam = Number(req.params.id);
 
-    res.send(updateArmadura);
+   if (!idParam) {
+     return res.status(400).send({ message: 'ID inválido' });
+   }
+
+  const armaduraEdit = req.body;
+
+  if (
+    !armaduraEdit ||
+    !armaduraEdit.nome ||
+    !armaduraEdit.descricao ||
+    !armaduraEdit.usuarios ||
+    !armaduraEdit.background ||
+    !armaduraEdit.imagem
+  ) {
+    return res.status(400).send({ message: 'Objeto incompleto!' });
+  }
+
+  const updateArmadura = armaduraService.updateArmaduraService(
+    idParam,
+    armaduraEdit,
+  );
+
+  res.send(updateArmadura);
 };
 
 const deleteArmaduraController = (req, res) => {
-    const idParam = Number(req.params.id);
-    armaduraService.deleteArmaduraService(idParam);
-    res.send({message: 'Armadura deletada com sucesso'})
-}
+  const idParam = Number(req.params.id);
 
+   if (!idParam) {
+     return res.status(400).send({ message: 'ID inválido' });
+   }
+
+  armaduraService.deleteArmaduraService(idParam);
+  res.send({ message: 'Armadura deletada com sucesso!' });
+};
 
 module.exports = {
   findAllArmadurasController,
